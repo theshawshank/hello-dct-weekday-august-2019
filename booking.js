40201ec5db444aafe2e8217c12fa66bdfaf771ce
Shashank Shetty <sshetty1@alumni.cmu.edu>
2020-01-10 16:23:54 +0530

    Changed all Booking.js functions

--- booking.js
+++ booking.js
@@ -1,3 +1,15 @@
-function booking(){
-    return "Tickets Booked";
+function bookingSum(n1, n2){
+    return n1+n2;
+}
+
+function bookingDifference(n1, n2){
+    return n1-n2;
+}
+
+function bookingMultiply(n1, n2){
+    return n1*n2;
+}
+
+function bookingDivide(n1, n2){
+    return n1/n2;
 }
